ebd3b0c0d59968305a8aff0a0e86bda35e3f1ad8
alesrv <srvf2019@gmail.com>
2025-07-09 11:34:58 +0200

    menu hamburguesa cierre X

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".hamburger")?.addEventListener("click", () => {
-    document.querySelector(".nav-links")?.classList.toggle("expanded");
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger?.addEventListener("click", () => {
+    navLinks?.classList.toggle("expanded");
+    hamburger.classList.toggle("open");
   });
 });
